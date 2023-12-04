3d60b5e9f6792f405261bb9deeaf5ac4d12cab46
arulvenkatesh1422 <aarulvenkatesh@gmail.com>
2023-12-04 10:24:31 +0530

    Test commit

---
+++ TestProject/src/Git/Project.java
@@ -0,0 +1,13 @@
+package Git;
+
+public class Project {
+
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("Hello");
+		
+		throw new Exception("This is exception");
+
+	}
+
+}
